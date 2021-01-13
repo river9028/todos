eed76a9103852b959f5b008f1c5b9e54320584db
DESKTOP-NQTKL8R\river k <river9028@gmail.com>
2021-01-14 01:52:28 +0900

    개발환경세팅

--- App.tsx
+++ App.tsx
@@ -1,21 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
+import Constants from 'expo-constants';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
+import { getStatusBarHeight } from 'react-native-status-bar-height';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<SafeAreaView style={{ flex: 1, backgroundColor: "pink" }}>
+				<StatusBar style="auto" />
+				<View style={styles.contents}>
+					<Text>Open up App.tsx to start working on your app!</Text>
+					<Text>{getStatusBarHeight()}</Text>
+
+				</View>
+			</SafeAreaView>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: 'pink',
+		paddingTop: getStatusBarHeight(),
+		paddingBottom: getBottomSpace(),
+		// alignItems: 'center',
+		// justifyContent: 'center',
+	},
+	contents: {
+		flex: 1,
+		backgroundColor: 'lightgreen',
+
+	}
 });
